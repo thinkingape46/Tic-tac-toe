5ee24075ada3b130a9089a7e03a5375a188c948b
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-18 18:49:33 +0530

    Update Tic-tac-toe_rework1.py
    Now, players are randomly chosen and asked for a choice of symbol.

--- Tic-tac-toe_rework1.py
+++ Tic-tac-toe_rework1.py
@@ -1,9 +1,9 @@
 from IPython.display import clear_output
+from random import randint
 ''' This list stores symbol choice of players. '''
-symbol_choice = ['', '']
 
 board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-
+symbol_choice = ['X', 'O']
 
 '''clear_output clears the board.'''
 clear_output
@@ -11,7 +11,7 @@ clear_output
 def print_board(board):
 
     '''This function prints the tic-tac-toe board.'''
-
+    clear_output()
     print(board[7]+'|'+board[8]+'|'+board[9])
     print(board[4]+'|'+board[5]+'|'+board[6])
     print(board[1]+'|'+board[2]+'|'+board[3])
@@ -19,13 +19,14 @@ def print_board(board):
 def tic_tac():
 
     '''players are asked whether they wish to play game?'''
+    
+    print("\nWELCOME TO TIC-TAC-TOE!\n")
 
     try:
         start = input("Do you wish to play the game (yes or no)? ")
     except:
         print("I am not using this, because the input accepts anything")
     if start in ['yes', 'oui']:
-        print("\nWELCOME TO TIC-TAC-TOE!\n")
         start_code()
     elif start in ['no', 'non']:
         print('See you later!')
@@ -36,7 +37,7 @@ def tic_tac():
 def tic_tac_next():
 
     '''upon completion of the game, players are given option to restart the game.'''
-    
+
     try:
         start = input("\nDo you wish to play the game again (yes or no)? ")
     except:
@@ -51,28 +52,50 @@ def tic_tac_next():
 
 def start_code():
 
-    '''player 1 is asked to choose their symbol between 'X' and 'O'.'''
+    toss = randint(0, 1)
 
-    player1_symbol = input("Player1, Enter your symbol X or O: ")
+    if toss == 0:
+        turn = "player1"
+    else:
+        turn = "player2"
+    
+    symbol = ''
 
-    if player1_symbol not in ['X', 'O']:
-        print("Plese enter the symbol 'X' or 'O'")
-        start_code()
-        
-    elif player1_symbol == 'X':
-        player2_symbol = 'O'
-        symbol_choice[0] = player1_symbol
-        '''Once the first player choose a symbol, the second player is assigned the second one inthe next line.'''
-        symbol_choice[1] = player2_symbol
-        print(f"\nPlayer1 is assigned {player1_symbol} and Player2 is assigned {player2_symbol}\n")
+    if turn == 'player1':
+
+        while symbol not in ('X', 'O'):
+
+            symbol = input(f"{turn}, Enter your choice of symbol X or O: ")
+            
+            if symbol == 'X':
+                symbol_choice[0] = 'X'
+                symbol_choice[1] = 'O'
+                
+            if symbol == 'O':
+                symbol_choice[0] = 'O'
+                symbol_choice[1] = 'X'
+            else:
+                continue
+
+        print(f"player1 is assigned {symbol_choice[0]} and player2 is assigned {symbol_choice[1]}")
         play_game()
 
-    elif player1_symbol == 'O':
-        player2_symbol = 'X'
-        symbol_choice[0] = player1_symbol
-        '''Once the first player choose a symbol, the second player is assigned the second one inthe next line.'''
-        symbol_choice[1] = player2_symbol
-        print(f"Player1 is assigned {player1_symbol} and Player2 is assigned {player2_symbol}")
+    elif turn == 'player2':
+
+        while symbol not in ('X', 'O'):
+
+            symbol = input(f"{turn}, Enter your choice of symbol X or O: ")
+            
+            if symbol == 'X':
+                symbol_choice[0] = 'O'
+                symbol_choice[1] = 'X'
+            if symbol == 'O':
+                symbol_choice[0] = 'X'
+                symbol_choice[1] = 'O'
+            else:
+                continue
+
+        print(f"player1 is assigned {symbol_choice[0]} and player2 is assigned {symbol_choice[1]}")
         play_game()
 
 def play_game():
@@ -94,7 +117,7 @@ def play_game():
             print_board(board)
             play_game()
         else:
-            board[player1_num] = (symbol_choice[0])
+            board[player1_num] = symbol_choice[0]
             clear_output()
             '''clear_output(), it is to refresh the screen. '''
 
@@ -138,7 +161,7 @@ def play_game2():
             play_game2()
 
         else:
-            board[player2_num] = (symbol_choice[1])
+            board[player2_num] = symbol_choice[1]
             clear_output()
             print_board(board)
 
