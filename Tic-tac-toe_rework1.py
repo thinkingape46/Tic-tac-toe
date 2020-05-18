86e0cc63caf5074d06e82067c99ae010434b4d17
thinkingape46 <jagadeesh.kumar2091@gmail.com>
2020-05-18 12:33:26 +0530

    update to code, see description
    1. DOCSTRINGs added.
    2. Player can use the French words 'oui' and 'non' in the game.
    3. Some other changes to improve pylint score.

--- Tic-tac-toe_rework1.py
+++ Tic-tac-toe_rework1.py
@@ -1,14 +1,16 @@
-# This list stores symbol choice of players.
-symbol_choice = ['','']
+from IPython.display import clear_output
+''' This list stores symbol choice of players. '''
+symbol_choice = ['', '']
 
 board = ['#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 
-from IPython.display import clear_output
-# clear_output clears the board.
+
+'''clear_output clears the board.'''
+clear_output
 
 def print_board(board):
 
-# This function prints the tic-tac-toe board.
+    '''This function prints the tic-tac-toe board.'''
 
     print(board[7]+'|'+board[8]+'|'+board[9])
     print(board[4]+'|'+board[5]+'|'+board[6])
@@ -16,16 +18,16 @@ def print_board(board):
 
 def tic_tac():
 
-# player are asked whether they wish to play game?
+    '''players are asked whether they wish to play game?'''
 
     try:
-        start = input("Do you wish to play the game (yes or no)?")
+        start = input("Do you wish to play the game (yes or no)? ")
     except:
         print("I am not using this, because the input accepts anything")
-    if start == 'yes':
+    if start in ['yes', 'oui']:
         print("\nWELCOME TO TIC-TAC-TOE!\n")
         start_code()
-    elif start == 'no':
+    elif start in ['no', 'non']:
         print('See you later!')
     else:
         print("please enter 'yes' or 'no' to continue...\n")
@@ -33,15 +35,15 @@ def tic_tac():
 
 def tic_tac_next():
 
-# upon completion of the game, players are given option to restart the game.
+    '''upon completion of the game, players are given option to restart the game.'''
     
     try:
-        start = input("\nDo you wish to play the game again (yes or no)?")
+        start = input("\nDo you wish to play the game again (yes or no)? ")
     except:
         print("I am not using this, because the input accepts anything")
-    if start == 'yes':
+    if start in ['yes', 'oui']:
         start_code()
-    elif start == 'no':
+    elif start in ['no', 'non']:
         print('See you later!')
     else:
         print("please enter 'yes' or 'no' to continue...\n")
@@ -49,17 +51,18 @@ def tic_tac_next():
 
 def start_code():
 
-# player 1 is asked to choose their symbol between 'X' and 'O'.
+    '''player 1 is asked to choose their symbol between 'X' and 'O'.'''
 
-    player1_symbol = input("Player1, Enter your symbol")
+    player1_symbol = input("Player1, Enter your symbol X or O: ")
 
-    if player1_symbol not in ['X','O']:
+    if player1_symbol not in ['X', 'O']:
         print("Plese enter the symbol 'X' or 'O'")
         start_code()
-
+        
     elif player1_symbol == 'X':
         player2_symbol = 'O'
         symbol_choice[0] = player1_symbol
+        '''Once the first player choose a symbol, the second player is assigned the second one inthe next line.'''
         symbol_choice[1] = player2_symbol
         print(f"\nPlayer1 is assigned {player1_symbol} and Player2 is assigned {player2_symbol}\n")
         play_game()
@@ -67,21 +70,24 @@ def start_code():
     elif player1_symbol == 'O':
         player2_symbol = 'X'
         symbol_choice[0] = player1_symbol
+        '''Once the first player choose a symbol, the second player is assigned the second one inthe next line.'''
         symbol_choice[1] = player2_symbol
         print(f"Player1 is assigned {player1_symbol} and Player2 is assigned {player2_symbol}")
         play_game()
 
 def play_game():
 
-# This function accepts input input from the player1.
+    '''This function accepts input input from the player1, and finishes the game if player1 wins or draw. Otherwise it will move on to play_game2(). '''
 
-    from IPython.display import clear_output
-    player1_choice = input("Player1, please enter your choice of number:")
-    if (player1_choice) not in ['1','2','3','4','5','6','7','8','9']:
-        print("Only numbers between 1 and 9 are allowed")
+    player1_choice = input("Player1, please enter your choice of number: ")
+
+    if int((player1_choice)) not in range(1, 10):
+        print("\nOnly numbers between 1 and 9 are allowed\n")
         play_game()
+
     else:
         player1_num = int(player1_choice)
+
         if board[player1_num] != ' ':
             print('The cell is already filled')
             clear_output()
@@ -90,44 +96,65 @@ def play_game():
         else:
             board[player1_num] = (symbol_choice[0])
             clear_output()
+            '''clear_output(), it is to refresh the screen. '''
+
             print_board(board)
+
             if ' ' not in board:
+                '''Checking if board is completely filled. no spaces ---> board is filled'''
                 print("This game is a tie, well done!")
                 for n in range(1,10):
                     board[n] = ' '
                 tic_tac_next()
-            elif board[7] == board[8] == board[9] != ' ' or board[1] == board[2] == board[3] != ' '  or board[4] == board[5] == board[6] != ' '  or board[7] == board[4] == board[1] != ' '  or board[8] == board[5] == board[2] != ' '  or board[9] == board[6] == board[3] != ' '  or board[7] == board[5] == board[3] != ' '  or board[9] == board[5] == board[1] != ' ' :
-                print("Player 1 has won, well done!\nPlayer 2, good luck next time.")
+
+            elif board[7] == board[8] == board[9] != ' ' or board[1] == board[2] == board[3] != ' '  or board[4] == board[5] == board[6] != ' '  or board[7] == board[4] == board[1] != ' '  or board[8] == board[5] == board[2] != ' '  or board[9] == board[6] == board[3] != ' '  or board[7] == board[5] == board[3] != ' '  or board[9] == board[5] == board[1] != ' ':
+                '''Checking player1 won the game.'''
+                print("\nPlayer 1 has won, well done!\nPlayer 2, good luck next time.\n")
                 for n in range(1,10):
+                    '''emptying the board for the next round.'''
                     board[n] = ' '
                 tic_tac_next()
+
             else:
                 play_game2()
+
 def play_game2():
-    from IPython.display import clear_output
-    player2_choice = input("Player2, please enter your choice of number")
-    if (player2_choice) not in ['1','2','3','4','5','6','7','8','9']:
-        print("Only numbers between 1 and 9 are allowed")
+
+    '''This function accepts input input from the player1, and finishes the game if player1 wins or draw. Otherwise it will move on to play_game2().'''
+    
+    player2_choice = int(input("Player2, please enter your choice of number: "))        
+
+    if int((player2_choice)) not in range(1, 10):
+        print("\nOnly numbers between 1 and 9 are allowed\n")
         play_game2()
+
     else:
         player2_num = int(player2_choice)
+
         if board[player2_num] != ' ':
             print('The cell is already filled')
             clear_output()
             print_board(board)
             play_game2()
+
         else:
             board[player2_num] = (symbol_choice[1])
             clear_output()
             print_board(board)
+
             if ' ' not in board:
+                '''Checking if board is completely filled. no spaces ---> board is filled'''
                 print("This game is a tie, well done!")
                 for n in range(1,10):
                     board[n] = ' '
                 tic_tac_next()
-            if board[7] == board[8] == board[9] != ' ' or board[1] == board[2] == board[3] != ' '  or board[4] == board[5] == board[6] != ' '  or board[7] == board[4] == board[1] != ' '  or board[8] == board[5] == board[2] != ' '  or board[9] == board[6] == board[3] != ' '  or board[7] == board[5] == board[3] != ' '  or board[9] == board[5] == board[1] != ' ' :
+
+            if board[7] == board[8] == board[9] != ' ' or board[1] == board[2] == board[3] != ' '  or board[4] == board[5] == board[6] != ' '  or board[7] == board[4] == board[1] != ' '  or board[8] == board[5] == board[2] != ' '  or board[9] == board[6] == board[3] != ' '  or board[7] == board[5] == board[3] != ' '  or board[9] == board[5] == board[1] != ' ':
+                '''Checking player2 won the game.'''
                 print("Player 2 has won, well done!\nPlayer 1, good luck next time.")
+
                 for n in range(1,10):
+                    '''emptying the board for the next round.'''
                     board[n] = ' '
                 tic_tac_next()
             else:
